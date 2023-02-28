3f72f4e5efa8f2df1a1750fd00004b2efbbf4439
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-02-28 16:43:16 +0200

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,6 +2,7 @@ import throttle from "lodash.throttle";
 
 const STORAGE_KEY = 'feedback-form-state';
 const formData = {};
+
 const refs = {
     form: document.querySelector('.feedback-form'),
     textarea: document.querySelector('.feedback-form textarea')
