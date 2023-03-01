00c27f5593bd3db76e7330a755b02f5aae383df6
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-03-01 12:28:50 +0200

    done task 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,36 +5,51 @@ const formData = {};
 
 const refs = {
     form: document.querySelector('.feedback-form'),
+    input: document.querySelector('.feedback-form input'),
     textarea: document.querySelector('.feedback-form textarea')
 }
 
 refs.form.addEventListener('submit', onFormSubmit);
+refs.input.addEventListener('input', throttle(onEmailInput, 500));
 refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
 refs.form.addEventListener('input', e => {
     formData[e.target.name] = e.target.value;
-    console.log(formData)
 });
 
 populateTextarea();
+populateInput();
 
 function onFormSubmit(e) {
     e.preventDefault();
-    console.log('отправить форму')
+    console.log(formData)
     e.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY)
 }
 
-function onTextareaInput(e) {
-    const message = e.target.value;
+function onEmailInput() {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+}
 
-    localStorage.setItem(STORAGE_KEY, message)
+function onTextareaInput() {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+}
+
+function populateInput() {
+    const savedEmail = localStorage.getItem(STORAGE_KEY);
+    const parsedSavedEmail = JSON.parse(savedEmail)
+    
+    if (savedEmail) {
+        
+        refs.input.value = parsedSavedEmail.email
+    }
 }
 
 function populateTextarea() {
     const savedMessage = localStorage.getItem(STORAGE_KEY);
+    const parsedSavedMessage = JSON.parse(savedMessage)
     
     if (savedMessage) {
         
-        refs.textarea.value = savedMessage;
+        refs.textarea.value = parsedSavedMessage.message
     }
 }
